d3ac416d0a522acb3825715dc36af11264a50edc
Muhamad Visat Sutarno <mvisat@gmail.com>
2016-10-07 15:37:54 +0700

    comment and toString

--- MyClassifier/src/myclassifier/MyJ48.java
+++ MyClassifier/src/myclassifier/MyJ48.java
@@ -15,6 +15,7 @@ public class MyJ48 extends Classifier {
     private Attribute splitAttribute;
     private double classValue;
     private double[] classDistribution;    
+    private Attribute classAttribute;
     private boolean pruning;    
     private double attributeThreshold;
 
@@ -36,7 +37,10 @@ public class MyJ48 extends Classifier {
 
     @Override
     public void buildClassifier(Instances instances) throws Exception {        
-        getCapabilities().testWithFail(instances);        
+        // Mengecek dapatkah classifier menghandle data
+        getCapabilities().testWithFail(instances);     
+        
+        // Isi missing values
         for (int i = 0; i < instances.numAttributes(); ++i) {
             Attribute attribute = instances.attribute(i);
             for (int j = 0; j < instances.numInstances(); ++j) {
@@ -45,7 +49,8 @@ public class MyJ48 extends Classifier {
                     instance.setValue(attribute, fillMissingValue(instances, attribute));
             }
         }
-        instances.deleteWithMissingClass();
+        // Hapus instance dengan missing class
+        instances.deleteWithMissingClass();        
         buildTree(instances);
     }
     
@@ -63,6 +68,33 @@ public class MyJ48 extends Classifier {
         }
         else return -1;        
     }    
+    
+    @Override
+    public String toString() {
+        if ((classDistribution == null) && (successors == null))
+            return "MyClassifier.MyJ48: No model";        
+        return "MyClassifier.MyJ48\n\n" + treeToString(0);  
+    }
+    
+    private String treeToString(int level) {
+        StringBuilder text = new StringBuilder();
+        if (splitAttribute == null) {
+            if (Instance.isMissingValue(classValue))
+                text.append(": null");
+            else
+                text.append(": ").append(classAttribute.value((int) classValue));
+        }
+        else {
+            for (int j = 0; j < splitAttribute.numValues(); j++) {
+                text.append("\n");
+                for (int i = 0; i < level; i++)
+                    text.append("|  ");
+                text.append(splitAttribute.name()).append(" = ").append(splitAttribute.value(j));
+                text.append(successors[j].treeToString(level + 1));
+            }
+        }
+        return text.toString();
+    }
         
     private double fillMissingValue(Instances instances, Attribute attribute) {
         int[] sum = new int[attribute.numValues()];
@@ -70,12 +102,13 @@ public class MyJ48 extends Classifier {
             sum[(int)instances.instance(i).value(attribute)]++;
         return sum[Util.indexOfMax(sum)];
     }
-    
+            
     public void setPruning(boolean enable){
         pruning = enable;
     }
 
     public void buildTree(Instances instances) throws Exception {        
+        // Lakukan pruning
         if (pruning)
             instances = prune(instances);
         
@@ -85,6 +118,7 @@ public class MyJ48 extends Classifier {
             classDistribution = new double[instances.numClasses()];
         }
         else {            
+            // Cari gain ratio maksimum dari atribut
             double[] gainRatio = new double[instances.numAttributes()];
             Enumeration attEnum = instances.enumerateAttributes();
             while (attEnum.hasMoreElements()) {
@@ -92,7 +126,9 @@ public class MyJ48 extends Classifier {
                 gainRatio[att.index()] = Util.calculateGainRatio(instances, att);
             }
             splitAttribute = instances.attribute(Util.indexOfMax(gainRatio));
-            
+                                    
+            // Jika gain ratio maksimum = 0, buat daun dengan label kelas mayoritas
+            // Jika tidak, buat successor
             if (Util.equalValue(gainRatio[splitAttribute.index()], 0)) {
                 splitAttribute = null;
                 classDistribution = new double[instances.numClasses()];
@@ -102,6 +138,7 @@ public class MyJ48 extends Classifier {
                 }
                 Util.normalizeClassDistribution(classDistribution);
                 classValue = Util.indexOfMax(classDistribution);                
+                classAttribute = instances.classAttribute();
             }
             else {
                 Instances[] splitData = Util.splitData(instances, splitAttribute);
